fb1450d5cd47e8c8b88795985765fde06c6b3de0
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-04 21:36:55 +0100

    after 1a

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 
+const Hello = (props) => {
+  return (
+    <>
+      <p>Hello, {props.name}, you look like you're {props.age} years old!</p>
+    </>
+  )
+}
+
 const App = () => {
   let now = new Date();
   let a = 10;
   let b= 20;
 
   return (
-    <div>
+    <>
+      <h1>
+        <Hello name="Marc" age={a + b}/>
+        <Hello name="Urte" age="10"/>
+      </h1>
       <p>Hello world, it is {now.toLocaleString()}</p>
       <p>
         {a} plus {b} is {a + b}
       </p>
-    </div>
+    </>
   )
 };
 
