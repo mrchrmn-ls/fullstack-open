be9b703c59c69a643c86894e0a5fc828c8db033f
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-06 23:52:48 +0100

    exercise 1d unicafe start

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,46 @@
 import React, { useState } from 'react'
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Feedback = () => {
+  const handleClickGood = () => setGood(good + 1);
+  const handleClickNeutral = () => setNeutral(neutral + 1);
+  const handleClickBad = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1> give feedback</h1>
+      <Button onClick={handleClickGood} text="good" />
+      <Button onClick={handleClickNeutral} text="neutral" />
+      <Button onClick={handleClickBad} text="bad" />
+    </>
+  )
+}
+
+const Stat = ({ name, count }) => <p>{name}: {count}</p>;
+
+const Statistics = ({ stats }) => {
+  return (
+    <>
+      <h1>
+      </h1>
+      <Stat name="good" count={stats.good} />
+      <Stat name="neutral" count={stats.neutral} />
+      <Stat name="bad" count={stats.bad} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
+      <Feedback />
+      <Statistics stats={{ good, neutral, bad }} />
     </div>
   );
 };
