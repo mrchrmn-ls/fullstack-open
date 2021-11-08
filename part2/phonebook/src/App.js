8a0440a6a964c0c2b36bc7f7c3bbd99e3730c2b3
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-08 16:45:22 +0100

    part2b ex2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,30 @@ import React, { useState } from 'react';
 
 const Person = ({ info }) => <p>{info.name} - {info.number}</p>
 
+const People = ({ persons }) => {
+  return (
+    <>
+      {
+        persons.map(person => <Person key={person.number} info={person} />)
+      }
+    </>
+  );
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
+  const [ search, setSearch ] = useState("");
+
+  const searchResult = (personArray, searchTerm) => {
+    searchTerm = searchTerm.toLowerCase();
+    return personArray.filter(person => {
+      return (person.name.toLowerCase().includes(searchTerm) || person.number.includes(searchTerm));
+    });
+  }; 
 
   const handleNameInput = (event) => {
     setNewName(event.target.value);
@@ -15,6 +33,10 @@ const App = () => {
 
   const handleNumberInput = (event) => {
     setNewNumber(event.target.value);
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
   }
 
   const addPerson = (event) => {
@@ -50,19 +72,21 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input onChange={handleSearch} value={search}/>
+      </div>
+      <h2>Add person or edit number</h2>
       <form onSubmit={addPerson}>
         <div>
           Name: <input onChange={handleNameInput} value={newName}/>
           Number: <input onChange={handleNumberInput} value={newNumber}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">add / edit</button>
         </div>
       </form>
       <h2>People</h2>
-      {
-        persons.map(person => <Person key={person.number} info={person} />)
-      }
+      <People persons={searchResult(persons, search)} />
     </div>
   );
 };
