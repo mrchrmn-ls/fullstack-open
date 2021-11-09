1b9b61c62e6aaa14be3945ded0558fa186e5d992
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-09 14:40:14 +0100

    remove async

--- part2/codealong-2d/src/services/notes.js
+++ part2/codealong-2d/src/services/notes.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 const baseURL = "http://localhost:3001/notes";
 
-async function getAll() {
+function getAll() {
   const req = axios.get(baseURL);
   return req.then(res => res.data);
 }
 
-async function create(newObject) {
+function create(newObject) {
   const req = axios.post(baseURL, newObject);
   return req.then(res => res.data);
 }
 
-async function update(id, newObject) {
+function update(id, newObject) {
   const req = axios.put(`${baseURL}/${id}`, newObject);
   return req.then(res => res.data);
 }
