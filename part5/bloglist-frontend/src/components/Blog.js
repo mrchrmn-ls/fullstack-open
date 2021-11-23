ef5893aa8a25fd52c034fd79b6cc096947487d58
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-23 14:35:25 +0100

    part5b ex7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import blogService from "../services/blogs";
 
 function Blog ({ blog }) {
+  const [ detailsVisible, setDetailsVisible ] = useState(false);
+
+  function toggleDetails() {
+    setDetailsVisible(!detailsVisible);
+  }
+
+  async function handleLike() {
+    console.log("liking");
+  }
+
   return (
-    <p>
-      {blog.title} - {blog.author}
-    </p>  
+    <>
+    <p>{blog.title} - {blog.author} - <button onClick={toggleDetails}>{detailsVisible ? "hide" : "show"} details</button>    </p>  
+    <ul style={{display: detailsVisible ? "" : "none"}}>
+      <li>{blog.url}</li>
+      <li>Likes: {blog.likes} <button onClick={handleLike}>like</button></li>
+    </ul>
+    </>
   )  
 }
 
