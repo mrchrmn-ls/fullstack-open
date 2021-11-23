9f4de4d7ff8d1961cb3f73442598073406a5708a
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-23 15:34:05 +0100

    part5b ex9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -60,7 +60,7 @@ const App = () => {
             <BlogForm state={{ blogs, setBlogs, setMessage, blogFormRef }}/>
           </Togglable>
           <div>
-            {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+            {blogs.slice().sort((a, b) => b.likes - a.likes).map(blog => <Blog key={blog.id} blog={blog} />)}
           </div>
         </div>
       }
