eea67948c6514e8d007962b23da66975030dc62f
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-25 19:33:27 +0100

    part5d added  cypress commands

--- part2/notes/cypress/integration/note_app.spec.js
+++ part2/notes/cypress/integration/note_app.spec.js
@@ -1,4 +1,4 @@
-describe("Note app", function() {
+describe("Note app", () => {
   beforeEach(() => {
     cy.request("POST", "http://localhost:3001/api/tests/reset");
     const user = {
@@ -10,48 +10,50 @@ describe("Note app", function() {
     cy.visit("http://localhost:3000");
   });
 
-  it("front page can be opened", () => {
-    cy.contains("Notes");
-    cy.contains("Note app, Department of Computer Science, University of Helsinki");
-  });
+  describe("basic funcionality", () => {
+    it("front page can be opened", () => {
+      cy.contains("Notes");
+      cy.contains("Note app, Department of Computer Science, University of Helsinki");
+    });
 
-  it("login form can be opened", () => {
-    cy.visit("http://localhost:3000");
-    cy.contains("login").click();
-  });
+    it("login form can be opened", () => {
+      cy.contains("login").click();
+    });
 
-  it("user can log in", () => {
-    cy.contains("login").click();
-    cy.get("#username").type("root");
-    cy.get("#password").type("fsosuperuser");
-    cy.get("#loginSubmit").click();
-    cy.contains("Superuser is logged in.");
-  });
-
-  it("login fails with wrong password", function() {
-    cy.contains("login").click();
-    cy.get("#username").type("mluukkai");
-    cy.get("#password").type("wrong");
-    cy.get("#loginSubmit").click();
-
-    cy.get(".error")
-      .should("contain", "Wrong credentials.")
-      .and("have.css", "color", "rgb(255, 0, 0)")
-      .and("have.css", "border-style", "solid");
-
-    cy.get("html").should("not.contain", "Matti Luukkainen logged in");
   });
 
 
-  describe("when logged in", function() {
-    beforeEach(function() {
+  describe("login handling", () => {
+    it("user can log in", () => {
       cy.contains("login").click();
       cy.get("#username").type("root");
       cy.get("#password").type("fsosuperuser");
       cy.get("#loginSubmit").click();
-      });
+      cy.contains("Superuser is logged in.");
+    });
 
-    it("a new note can be created", function() {
+    it("login fails with wrong password", () => {
+      cy.contains("login").click();
+      cy.get("#username").type("mluukkai");
+      cy.get("#password").type("wrong");
+      cy.get("#loginSubmit").click();
+
+      cy.get(".error")
+        .should("contain", "Wrong credentials.")
+        .and("have.css", "color", "rgb(255, 0, 0)")
+        .and("have.css", "border-style", "solid");
+
+      cy.get("html").should("not.contain", "Matti Luukkainen logged in");
+    });
+  });
+
+
+  describe("when logged in", () => {
+    beforeEach(() => {
+      cy.login("root", "fsosuperuser");
+    });
+
+    it("a new note can be created", () => {
       cy.contains("new note").click();
       cy.get("#newNoteInput").type("a note created by cypress");
       cy.contains("save").click();
@@ -59,14 +61,15 @@ describe("Note app", function() {
     });
 
 
-    describe("and a note exists", function () {
-      beforeEach(function () {
-        cy.contains("new note").click();
-        cy.get("#newNoteInput").type("another note from cypress");
-        cy.contains("save").click();
+    describe("and a note exists", () => {
+      beforeEach(() => {
+        cy.createNote({
+          content: "another note from cypress",
+          important: false
+        });
       });
 
-      it("it can be made important", function () {
+      it("it can be made important", () => {
         cy.contains("another note from cypress")
           .contains("make important")
           .click();
