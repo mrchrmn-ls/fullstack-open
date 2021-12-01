00051da20864f47d67b3ad6712ec1745f3f8bc4b
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-01 22:20:14 +0100

    part9b first block done

--- part9/multiplier.ts
+++ part9/multiplier.ts
@@ -1,5 +1,36 @@
-function multiplicator(a: number, b: number, printText: string) {
-  console.log(printText, a * b);
+interface MultiplyValues {
+  value1: number;
+  value2:number;
 }
 
-multiplicator(2, 4, "Result:");
+
+function parseArguments(args: Array<string>): MultiplyValues {
+  if (args.length < 4) throw new Error("Not enough arguments.");
+  if (args.length > 4) throw new Error("Too many arguments.");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error("You need to provide two _numbers_.")
+  }
+}
+
+
+function multiplicator(a: number, b: number, printText: string) {
+  console.log(printText,  a * b);
+}
+
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
+} catch(error: unknown) {
+  let errorMessage = "Something went wrong."
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
