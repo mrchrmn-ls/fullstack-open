6862cd349d67f0e5c451f350b0280e0f12d394f9
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-01 18:26:07 +0100

    added types for nodes

---
+++ part9/calculator.ts
@@ -0,0 +1,28 @@
+type Operation = "multiply" | "add" | "divide";
+
+
+function calculator(a: number, b: number, op: Operation): number {
+  switch(op) {
+    case "multiply":
+      return a * b;
+    case "add":
+      return a + b;
+    case "divide":
+      if (b === 0) throw new Error("cannot divide by 0!");
+      return a / b;
+    default:
+      throw new Error("unknown operation");
+  }
+}
+
+
+try {
+  console.log(calculator(1, 5, "divide"));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
+
